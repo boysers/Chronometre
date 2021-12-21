439ca1e7482ddd0f017d03137000cd5b8e61da58
boysers <boyser2015@gmail.com>
2021-12-21 18:36:04 +0100

    ajout des commentaires

--- scripts/app.js
+++ scripts/app.js
@@ -1,7 +1,7 @@
 import Chrono from "./chrono.js";
 
 import Input from "./input.js";
-const { addQuerySelector, add, reset } = Input;
+const { addQuerySelector, add, reset, animateOfButton } = Input;
 
 // ---
 class NewChrono extends Chrono {
@@ -9,17 +9,17 @@ class NewChrono extends Chrono {
     super();
 
     this.button = addQuerySelector(["start", "pause", "stop"]);
-
     this.input = addQuerySelector(["heure", "minute", "seconde"]);
+
     this.watchTimer = () => {
       add(this.time, this.input);
     };
   }
 
-  watch(buttonID) {
-    if (this.chronoPause && buttonID === "pause") return;
+  watch(buttonActiveID) {
+    if (this.chronoPause && buttonActiveID === "pause") return;
 
-    switch (buttonID) {
+    switch (buttonActiveID) {
       case "start":
         this.start();
         break;
@@ -31,25 +31,14 @@ class NewChrono extends Chrono {
         reset(this.input);
     }
 
-    this.animateOfButton(buttonID);
-  }
-
-  animateOfButton(buttonID) {
-    let button = this.button;
-    for (let i in button) {
-      if (button[i].id && buttonID != button[i].id) {
-        button[i].classList.remove("btn-active");
-      } else {
-        button[i].classList.add("btn-active");
-      }
-    }
+    animateOfButton(buttonActiveID, button);
   }
 }
 
 const newChrono = new NewChrono();
 // ---
 
-const button = addQuerySelector(["start", "pause", "stop"]);
+const button = newChrono.button;
 
 button.start.addEventListener("click", () => {
   newChrono.watch("start");
--- scripts/input.js
+++ scripts/input.js
@@ -1,20 +1,29 @@
+/**
+ * Boite à outil pour intéragire avec le DOM
+ */
 export default class Input {
-  constructor() {
-    this.buttonID;
-  }
   /**
-   * document.querySelector(`#toto`)
+   * Ajoute dans un objet les éléments avec comme nom son id |
+   * `document.querySelector("#id")`
    * @param {array} array [ ]
-   * @returns {object} Object.element#toto
+   * @returns {object} Object
    */
   static addQuerySelector(array = []) {
-    let inputs = {};
+    let object = {};
+
     for (let id of array) {
-      inputs[id] = document.querySelector(`#${id}`);
+      object[id] = document.querySelector(`#${id}`);
     }
-    return inputs;
+    return object;
   }
 
+  /**
+   * Pour le Chronomètre :
+   * Ajoute les valeurs de l'objet time aux éléments :
+   * `input.heure`, `input.minute`, `input.seconde`
+   * @param {object} time
+   * @param {object} input
+   */
   static add(time, input) {
     let i = input;
     let t = time;
@@ -24,11 +33,34 @@ export default class Input {
     i.seconde.value = t.sec < 10 ? "0" + t.sec : t.sec;
   }
 
+  /**
+   * Pour le Chronomètre :
+   * Retire les valeurs (value) des éléments (input) :
+   * `input.heure`, `input.minute`, `input.seconde`
+   * @param {object} input
+   */
   static reset(input) {
     let i = input;
 
     i.heure.value = "";
     i.minute.value = "";
     i.seconde.value = "";
+  }
+
+  /**
+   * Ajoute la class css btn-active du bouton et la retire pour les autres,
+   * @param {string} buttonActiveID Bouton actif
+   * @param {object} buttonList Liste des boutons
+   */
+  static animateOfButton(buttonActiveID, buttonList) {
+    let button = buttonList;
+
+    for (let i in button) {
+      if (button[i].id && buttonActiveID != button[i].id) {
+        button[i].classList.remove("btn-active");
+      } else {
+        button[i].classList.add("btn-active");
+      }
+    }
   }
 }
